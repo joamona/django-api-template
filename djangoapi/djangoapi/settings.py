944261c7c388f155b208ad82bcbe0220445e7365
joamona <joamona@cgf.upv.es>
2025-03-13 12:16:34 +0100

    Commented csrf middleware

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -58,7 +58,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+#Comment this to avoid CSRF tokens as we are using Django as rest API
+#    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
