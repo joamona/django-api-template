5b5f2b29d735cf29748b6ff791609509f85652d7
joamona <joamona@cgf.upv.es>
2025-03-31 12:50:25 +0200

    m

--- djangoapi/buildings/admin.py
+++ djangoapi/buildings/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.contrib.gis import admin
 from .models import Buildings, Owners
 
-admin.site.register(Buildings, admin.GISModelAdmin)
 admin.site.register(Owners, admin.ModelAdmin)
+admin.site.register(Buildings, admin.GISModelAdmin)
