166c06a9f1656d82ff39a2bd5f350013cd63c3c3
joamona <joamona@cgf.upv.es>
2025-05-19 09:48:28 +0200

    m

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
 if DEBUG:
     #CORS_ALLOW_ALL_ORIGINS = True   <-- Not allowed any more for chrome
     #You need to specify the allowed origins
-    CORS_ALLOWED_ORIGINS=['http://localhost:8888', 'http://localhost:4200']
+    CORS_ALLOWED_ORIGINS=['http://localhost:4200']
 
 #necressary to allow the cookies to be sent in the header of the request
 CORS_ALLOW_CREDENTIALS = True
