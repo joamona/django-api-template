775e1542a332f18ac1c2516122475b6eb34e9b43
joamona <joamona@cgf.upv.es>
2026-04-29 11:33:18 +0200

    Added configuration to manage admin static files

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -195,7 +195,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#Archivos estáticos para el sitio de administración de Django
+#1. Dónde se crean con el comando python manage.py collectstatic
+STATIC_ROOT= os.getenv('STATIC_ROOT','static_root')
+#2. Dónde se encuentran en la web
+STATIC_URL = os.getenv('STATIC_URL','static')   
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
