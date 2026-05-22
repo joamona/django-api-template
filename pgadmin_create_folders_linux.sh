6a4f7c47cc5f683ddaf43ff55f2adcc3240d362c
joamona <joamona@cgf.upv.es>
2026-05-22 14:08:18 +0200

    Configuration for pgadmin

--- pgadmin_create_folders_linux.sh
+++ pgadmin_create_folders_linux.sh
@@ -2,9 +2,13 @@ mkdir pgadmin4
 mkdir pgadmin4/data
 mkdir pgadmin4/servers
 touch pgadmin4/servers/servers.json
+mkdir pgadmin4/backup
+
 mkdir pgadmin4_prod
 mkdir pgadmin4_prod/data
 mkdir pgadmin4_prod/servers
 touch pgadmin4_prod/servers/servers.json
+mkdir pgadmin4_prod/backup
+
 sudo chown -R 5050:5050 pgadmin4
 sudo chown -R 5050:5050 pgadmin4_prod
--- pgadmin_create_folders_windows.bat
+++ pgadmin_create_folders_windows.bat
@@ -2,12 +2,14 @@
 mkdir pgadmin4
 mkdir pgadmin4\data
 mkdir pgadmin4\servers
+mkdir pgadmin4\backup
 type nul > pgadmin4\servers\servers.json
 
 :: Crear carpetas y archivos para pgadmin4_prod
 mkdir pgadmin4_prod
 mkdir pgadmin4_prod\data
 mkdir pgadmin4_prod\servers
+mkdir pgadmin4\backup
 type nul > pgadmin4_prod\servers\servers.json
 
 echo Operacion completada.
